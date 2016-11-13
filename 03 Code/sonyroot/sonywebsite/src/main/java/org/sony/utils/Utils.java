ea865f0dda610524f1dbbe05dcc339d8fb58eecd
sonyCheng <sonyCheng1988@163.com>
2016-11-13 08:53:02 +0800

    new

---
+++ 03 Code/sonyroot/sonywebsite/src/main/java/org/sony/utils/Utils.java
@@ -0,0 +1,15 @@
+package org.sony.utils;
+
+/**
+ *
+ * @author sonyCheng
+ * @version [V100R001C00 2016年11月13日]
+ * @see [类/声明]
+ */
+public class Utils
+{
+	public static void main(String[]args)
+	{
+		System.out.println();
+	}
+}
